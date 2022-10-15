1ed601059637a47b6ea5b65df2768bbb9dd5a21c
alaxhenry <alaxhenry0121@gmail.com>
2022-10-15 10:31:45 +0900

    Initial Commit

---
+++ QuizUniversity/QuizUniversityApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizUniversityApp.swift
+//  QuizUniversity
+//
+//  Created by Seungchul Ha on 2022/10/15.
+//
+
+import SwiftUI
+
+@main
+struct QuizUniversityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuizUniversity/View/QuizView.swift
@@ -0,0 +1,20 @@
+//
+//  QuizView.swift
+//  QuizUniversity
+//
+//  Created by Seungchul Ha on 2022/10/15.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QuizView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuizView()
+    }
+}
